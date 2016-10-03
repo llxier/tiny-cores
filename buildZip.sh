550ee7e71c75504df3f1193e3888e4eed6a37938
Lefteris Alexantonakis <lefteris.alexantonakis@gmail.com>
2016-10-03 10:53:06 +0300

    Add ATTinyx5

--- buildZip.sh
+++ buildZip.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-zip -r attiny13.zip external/attiny13/
-sha2 -256 attiny13.zip
+zip -r attiny13.zip external/attiny13/attiny13
+sha256sum attiny13.zip
 wc -c attiny13.zip
+
+zip -r attiny85.zip external/attiny
+sha256sum attiny85.zip
+wc -c attiny85.zip
+
